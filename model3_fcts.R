0a93ecaf0c146a79feb26f96b6ec16e9ed696427
JonBas <jonathan.bastard@pasteur.fr>
2019-05-09 14:30:06 +0700

    Added observation process, changed Model 3

--- model3_fcts.R
+++ model3_fcts.R
@@ -1,6 +1,6 @@
 # Model 3 functions
 
-eq_mod=function(t, state, param){
+eq_mod_3=function(t, state, param){
   
   beta = param[[1]]
   gamma = param[[2]]
@@ -31,4 +31,24 @@ obs_process=function(state, param){
   Re = pS * n_samp_S/(n_samp_S + n_samp_R) + pR * n_samp_R/(n_samp_S + n_samp_R)
   
   return(Re)
+}
+
+pred_mod_3 = function(farm, param){
+  beta = param[[1]]
+  gamma = param[[2]]
+  alpha = param[[3]]
+  ab_expo_farm = param[[4]]
+  pS = param[[5]] # Proportion of resistant bacteria in "S" chickens
+  pR = param[[6]] # Proportion of resistant bacteria in "R" chickens
+  
+  init_prev = round(obs[farm, 1]*200)
+  pred_indiv = ode(c(200-init_prev, chick_prev), 1:n_weeks, eq_mod_3, list(beta, gamma, alpha, ab_expo[farm,]))
+  
+  # Observation process:
+  pred = rep(NA, n_weeks)
+  for(t in 1:n_weeks){
+    pred[t] = obs_process(pred_indiv[t,2:3], list(50, 0, 1))
+  }
+  
+  return(pred)
 }
